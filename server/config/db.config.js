cdcdb74ab2b6f55341294be73213caf279d08f8f
pa45h <parth.katariya87@gmail.com>
2026-01-03 11:27:47 +0530

    env

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -3,17 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-exports.connectDB = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("MongoDB connected successfully");
-    })
-    .catch((err) => {
-      console.error("MongoDB connection error:", err);
-      process.exit(1);
-    });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
+
+export default connectDB;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.config.js";  
+import connectDB from "./config/db.config.js";
 
 
 dotenv.config();
